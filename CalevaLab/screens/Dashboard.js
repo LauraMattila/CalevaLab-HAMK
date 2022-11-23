683b36c44e5d68ddb90d92fd7d347bfea720cc52
jerejalmari <jere20111@student.hamk.fi>
2022-11-23 15:46:43 +0200

    getActivity ++ things

--- CalevaLab/screens/Dashboard.js
+++ CalevaLab/screens/Dashboard.js
@@ -46,7 +46,7 @@ const Dashboard = ({navigation}) => {
   const [endTime, setEndTime] = useState();
   const [loading, setLoading] = useState(true);
 
-  const [userId, setUserId] = useState('1');
+  const [userId, setUserId] = useState('2');
   const [accessToken, setAccessToken] = useState('');
 
   useEffect(() => {
