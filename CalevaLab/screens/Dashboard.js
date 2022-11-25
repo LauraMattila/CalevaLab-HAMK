56465736052a808ebb616253197bb464bb5fd61f
Sampo15 <75014754+Sampo15@users.noreply.github.com>
2022-11-25 09:33:44 +0200

    Update Dashboard.js

--- CalevaLab/screens/Dashboard.js
+++ CalevaLab/screens/Dashboard.js
@@ -65,7 +65,7 @@ const Dashboard = ({navigation}) => {
   const [endTime, setEndTime] = useState();
   const [loading, setLoading] = useState(true);
 
-  const [userId, setUserId] = useState('4');
+  const [userId, setUserId] = useState('1');
 
 
   const [accessToken, setAccessToken] = useState('');
@@ -74,7 +74,6 @@ const Dashboard = ({navigation}) => {
   var startdate = new Date();
   startdate.setDate(today.getDate() - 7);
 
-  const [userId, setUserId] = useState('3');
 
 
   var getDateArray = function (startdate, today) {
@@ -243,9 +242,11 @@ const Dashboard = ({navigation}) => {
             </View>
           )}
         />
-
       </View>
+      </View>
+    
     </PaperProvider>
+   
   );
 };
 
