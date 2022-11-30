69ffc22484a4ba2b3aea4ad5a06cf2f9aa60029b
Sampo15 <75014754+Sampo15@users.noreply.github.com>
2022-11-30 14:41:53 +0200

    Some prioritization random stuff

--- CalevaLab/db/UserDb.js
+++ CalevaLab/db/UserDb.js
@@ -1,57 +1,73 @@
 import firestore, {firebase} from '@react-native-firebase/firestore';
 
 export async function fetchStepPreference(id) {
-    try {
-      var userId = await firestore()
-        .collection('users')
-        .where('user_id', '==', id)
-        .get();
-      if (userId.empty) {
-        console.log('EI OLE USERID');
-        return;
-      }
-      userId.forEach(doc => {
-        result = doc.data().steps_preference;
-      });
-      return result;
-    } catch (error) {
-      console.error(error);
+  try {
+    var userId = await firestore()
+      .collection('users')
+      .where('user_id', '==', id)
+      .get();
+    if (userId.empty) {
+      console.log('EI OLE USERID');
+      return;
     }
+    userId.forEach(doc => {
+      result = doc.data().steps_preference;
+    });
+    return result;
+  } catch (error) {
+    console.error(error);
   }
+}
 
-  export async function fetchCaloriesPreference(id) {
-    try {
-      var userId = await firestore()
-        .collection('users')
-        .where('user_id', '==', id)
-        .get();
-      if (userId.empty) {
-        console.log('EI OLE USERID');
-        return;
-      }
-      userId.forEach(doc => {
-        result = doc.data().calories_preference;
-      });
-      return result;
-    } catch (error) {
-      console.error(error);
+export async function fetchCaloriesPreference(id) {
+  try {
+    var userId = await firestore()
+      .collection('users')
+      .where('user_id', '==', id)
+      .get();
+    if (userId.empty) {
+      console.log('EI OLE USERID');
+      return;
     }
+    userId.forEach(doc => {
+      result = doc.data().calories_preference;
+    });
+    return result;
+  } catch (error) {
+    console.error(error);
   }
-  export async function fetchSleepPreference(id) {
-    try {
-      var userId = await firestore()
-        .collection('users')
-        .where('user_id', '==', id)
-        .get();
-      if (userId.empty) {
-        console.log('EI OLE USERID');
-        return;
-      }
-      userId.forEach(doc => {
-        result = doc.data().sleep_preference;
-      });
-      return result;
-    } catch (error) {
-      console.error(error);
+}
+export async function fetchSleepPreference(id) {
+  try {
+    var userId = await firestore()
+      .collection('users')
+      .where('user_id', '==', id)
+      .get();
+    if (userId.empty) {
+      console.log('EI OLE USERID');
+      return;
     }
+    userId.forEach(doc => {
+      result = doc.data().sleep_preference;
+    });
+    return result;
+  } catch (error) {
+    console.error(error);
   }
+}
+
+export async function setSleepPreference(docid, sleepPref) {
+  try {
+
+ 
+    var res = await firestore()
+      .collection('users')
+      .doc(docid)
+      .update({"sleep_preference": sleepPref});
+
+      
+  
+  } catch (error) {
+    console.error(error);
+  }
+}
--- CalevaLab/screens/Userprofile.js
+++ CalevaLab/screens/Userprofile.js
@@ -1,14 +1,30 @@
 import React, {useState, useEffect, Component} from 'react';
+import {PermissionsAndroid} from 'react-native';
 
-import {StyleSheet, Text, View, Pressable} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  SafeAreaView,
+  TouchableHighlight,
+} from 'react-native';
+
+import {
+  fetchCaloriesPreference,
+  fetchStepPreference,
+  fetchSleepPreference,
+  setSleepPreference,
+} from '../db/UserDb';
 
 //import Icon from 'react-native-vector-icons/AntDesign';
 import SwitchSelector from 'react-native-switch-selector';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {Modal, Provider as PaperProvider} from 'react-native-paper';
 import {DataTable} from 'react-native-paper';
 import Moment from 'moment';
 
 import {RadioButton} from 'react-native-paper';
+import {CameraScreen} from 'react-native-camera-kit';
 
 const Userprofile = ({navigation}) => {
   const [firstname, setFirstname] = useState('Matti');
@@ -16,12 +32,48 @@ const Userprofile = ({navigation}) => {
   const [age, setAge] = useState('55');
   const [gender, setGender] = useState('Male');
 
-  const [Sleepchecked, setSleepChecked] = React.useState('sleepfit');
-  const [Stepschecked, setStepsChecked] = React.useState('stepsfit');
-  const [Calschecked, setCalsChecked] = React.useState('calsfit');
+  const [Sleepchecked, setSleepChecked] = React.useState('');
+  const [Stepschecked, setStepsChecked] = React.useState('');
+  const [Calschecked, setCalsChecked] = React.useState('');
+
+  
+  const [visibility, setVisibility] = useState(false);
+  const [doc, setDoc] = useState('sPuf8tq1glz8675jyJsA');
+  const [userId, setUserId] = useState('1');
+
+
+  const setSleep = async () => {
+    var preference = await fetchSleepPreference(userId);
+    switch (preference) {
+      case 'Polar':
+        var sleep = 'Fitbit';
+        setSleepChecked('sleepfit');
+        await setSleepPreference(doc, sleep);
+
+        break;
+   
+    }
+  };
+
+  const setSleep2 = async () => {
+    var preference = await fetchSleepPreference(userId);
+    switch (preference) {
+      case 'Fitbit':
+        var sleep = 'Polar';
+        setSleepChecked('sleeppolar');
+        await setSleepPreference(doc, sleep);
+
+        break;
+   
+    }
+  };
+
+
 
   return (
     <View>
+    
+
       <View style={styles.infocont}>
         <View>
           <Text style={styles.name}>
@@ -34,6 +86,7 @@ const Userprofile = ({navigation}) => {
         </View>
       </View>
 
+      
       <DataTable style={styles.datacont}>
         <DataTable.Header>
           <DataTable.Title>Service</DataTable.Title>
@@ -49,7 +102,7 @@ const Userprofile = ({navigation}) => {
               <RadioButton
                 value="sleeppolar"
                 status={Sleepchecked === 'sleeppolar' ? 'checked' : 'unchecked'}
-                onPress={() => setSleepChecked('sleeppolar')}
+                onPress={() => setSleep2()}
               />
             </View>
           </DataTable.Cell>
@@ -80,7 +133,7 @@ const Userprofile = ({navigation}) => {
               <RadioButton
                 value="sleepfit"
                 status={Sleepchecked === 'sleepfit' ? 'checked' : 'unchecked'}
-                onPress={() => setSleepChecked('sleepfit')}
+                onPress={() => setSleep()}
               />
             </View>
           </DataTable.Cell>
@@ -108,6 +161,31 @@ const Userprofile = ({navigation}) => {
   );
 };
 const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  textStyle: {
+    color: 'black',
+    fontSize: 16,
+    textAlign: 'center',
+    padding: 10,
+    marginTop: 16,
+  },
+  buttonStyle: {
+    fontSize: 16,
+    color: 'white',
+    backgroundColor: 'green',
+    padding: 5,
+    marginTop: 32,
+    minWidth: 250,
+  },
+  buttonTextStyle: {
+    padding: 5,
+    color: 'white',
+    textAlign: 'center',
+  },
   infocont: {
     marginVertical: 150,
     flexDirection: 'row',
