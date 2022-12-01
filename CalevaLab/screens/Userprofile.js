d0f401b6eeff7484f1c53e9998eb15ec7050d4e6
JanetteLehto <75020018+JanetteLehto@users.noreply.github.com>
2022-12-01 16:35:15 +0200

    Revert "Merge pull request #48 from LauraMattila/Janette"
    This reverts commit 1a747d35e74280ddc73d819ed7adc027ef136cfd, reversing
    changes made to ad1c19c88ac7d83d702dab73863e3a25e39179ab.

--- CalevaLab/screens/Userprofile.js
+++ CalevaLab/screens/Userprofile.js
@@ -6,21 +6,8 @@ import {
   Text,
   View,
   Pressable,
-
-  Image,
-  Platform,
-  PermissionsAndroid,
+  SafeAreaView,
   TouchableHighlight,
-  TouchableOpacity,
-} from 'react-native';
-import ImagePicker from 'react-native-image-picker';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-
-//import Icon from 'react-native-vector-icons/AntDesign';
-import SwitchSelector from 'react-native-switch-selector';
-import {Button, Provider as PaperProvider} from 'react-native-paper';
-
-
 } from 'react-native';
 
 import {
@@ -32,6 +19,8 @@ import {
   setCaloriesPreference
 } from '../db/UserDb';
 
+//import Icon from 'react-native-vector-icons/AntDesign';
+import SwitchSelector from 'react-native-switch-selector';
 import {Modal, Provider as PaperProvider} from 'react-native-paper';
 import {DataTable} from 'react-native-paper';
 import Moment from 'moment';
@@ -39,25 +28,11 @@ import Moment from 'moment';
 import {RadioButton} from 'react-native-paper';
 import {CameraScreen} from 'react-native-camera-kit';
 
-import {
-  fetchCaloriesPreference,
-  fetchStepPreference,
-  fetchSleepPreference,
-} from '../db/UserDb';
-
 const Userprofile = ({navigation}) => {
-  const kuka = [
-    {label: 'Sampo', value: '1'},
-    {label: 'Jere', value: '2'},
-    {label: 'Janette', value: '3'},
-    {label: 'Laura', value: '4'},
-  ];
-
   const [firstname, setFirstname] = useState('Matti');
   const [lastname, setLastname] = useState('Meikäläinen');
   const [age, setAge] = useState('55');
   const [gender, setGender] = useState('Male');
-
 
   const [Sleepchecked, setSleepChecked] = React.useState('');
   const [Stepschecked, setStepsChecked] = React.useState('');
@@ -123,169 +98,23 @@ const Userprofile = ({navigation}) => {
 
 
 
-  const [filePath, setFilePath] = useState({});
-  const [filePathh, setFilePathh] = useState(
-    'file:///data/user/0/com.calevalab/cache/rn_image_picker_lib_temp_eb789d88-e013-49fe-8cee-7ff5df0ee410.jpg',
-  );
-
-  const requestCameraPermission = async () => {
-    if (Platform.OS === 'android') {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.CAMERA,
-          {
-            title: 'Camera Permission',
-            message: 'App needs camera permission',
-          },
-        );
-        // If CAMERA Permission is granted
-        return granted === PermissionsAndroid.RESULTS.GRANTED;
-      } catch (err) {
-        console.warn(err);
-        return false;
-      }
-    } else return true;
-  };
-
-  const requestExternalWritePermission = async () => {
-    if (Platform.OS === 'android') {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-          {
-            title: 'External Storage Write Permission',
-            message: 'App needs write permission',
-          },
-        );
-        // If WRITE_EXTERNAL_STORAGE Permission is granted
-        return granted === PermissionsAndroid.RESULTS.GRANTED;
-      } catch (err) {
-        console.warn(err);
-        alert('Write permission err', err);
-      }
-      return false;
-    } else return true;
-  };
-
-  const captureImage = async type => {
-    let options = {
-      mediaType: type,
-      maxWidth: 300,
-      maxHeight: 550,
-      quality: 1,
-      videoQuality: 'low',
-      durationLimit: 30, //Video max duration in seconds
-      saveToPhotos: true,
-    };
-
-    let isCameraPermitted = await requestCameraPermission();
-    let isStoragePermitted = await requestExternalWritePermission();
-    if (isCameraPermitted && isStoragePermitted) {
-      launchCamera(options, response => {
-        if (response.didCancel) {
-          console.log('User cancelled image picker');
-        } else if (response.error) {
-          console.log('ImagePicker Error: ', response.error);
-        } else if (response.customButton) {
-          console.log('User tapped custom button: ', response.customButton);
-        } else
-          response['assets'].forEach(item => {
-            const base64 = item.base64;
-            const uri = item.uri;
-            const width = item.width;
-            const height = item.height;
-            const fileSize = item.fileSize;
-            const type = item.type;
-            const fileName = item.fileName;
-
-            setFilePathh(uri);
-
-            console.log('FILEPATH:                 ' + uri);
-          });
-      });
-    }
-  };
-
-  const chooseFile = type => {
-    let options = {
-      mediaType: type,
-      maxWidth: 300,
-      maxHeight: 550,
-      quality: 1,
-    };
-    launchImageLibrary(options, response => {
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      } else
-        response['assets'].forEach(item => {
-          const base64 = item.base64;
-          const uri = item.uri;
-          const width = item.width;
-          const height = item.height;
-          const fileSize = item.fileSize;
-          const type = item.type;
-          const fileName = item.fileName;
-
-          setFilePathh(uri);
-
-          console.log('FILEPATH:                 ' + filePathh);
-        });
-    });
-  };
-
   return (
     <View>
-
-      <SwitchSelector
-        options={kuka}
-        initial={0}
-        onPress={value => setUserId(value)}
-      />
-
     
 
       <View style={styles.infocont}>
-
         <View>
-          <View style={styles.container}>
-            <Image
-              style={{width: 120, height: 120, borderRadius: 75}}
-              source={{uri: filePathh}}
-              resizeMode={'cover'} // cover or contain its upto you view look
-            />
-            <TouchableOpacity
-              activeOpacity={0.5}
-              style={styles.buttonStyle}
-              onPress={() => captureImage('photo')}>
-              <Text style={styles.textStyle}> Take a photo</Text>
-            </TouchableOpacity>
+          <Text style={styles.name}>
+            {firstname} {lastname}
+          </Text>
 
-            <TouchableOpacity
-              activeOpacity={0.5}
-              style={styles.buttonStyle}
-              onPress={() => chooseFile('photo')}>
-              <Text style={styles.textStyle}> Choose Image</Text>
-            </TouchableOpacity>
-          </View>
+          <Text style={styles.info}>
+            {age} | {gender}
+          </Text>
         </View>
-        <View style={styles.infocont}>
-          <View>
-            <Text style={styles.name}>
-              {firstname} {lastname}
-            </Text>
+      </View>
 
-
-            <Text style={styles.info}>
-              {age} | {gender}
-            </Text>
-          </View>
-        </View>
-
-        
+      
       <DataTable style={styles.datacont}>
         <DataTable.Header>
           <DataTable.Title>Service</DataTable.Title>
@@ -356,7 +185,6 @@ const Userprofile = ({navigation}) => {
           </DataTable.Cell>
         </DataTable.Row>
       </DataTable>
-
     </View>
   );
 };
@@ -387,9 +215,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   infocont: {
-    marginVertical: 100,
+    marginVertical: 150,
     flexDirection: 'row',
-    height: 20,
+    height: 70,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -402,26 +230,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginHorizontal: 20,
     fontSize: 13,
-  },
-
-  sharecont: {
-    alignItems: 'center',
-    marginVertical: 60,
-  },
-
-  sharebutton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 100,
-    backgroundColor: '#1e90ff',
-    width: 100,
-    height: 100,
-  },
-  sharetext: {
-    fontSize: 20,
-    fontWeight: 'bold',
   },
 });
 export default Userprofile;
